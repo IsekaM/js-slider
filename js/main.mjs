7a8fc41fd46262d62a1044db5034aff1513abd5c
Makesi Morrison <makmorrison@gmail.com>
2020-01-08 19:39:35 -0500

    Bundled/minified JS and Css with parcel

--- js/main.mjs
+++
@@ -1,3 +0,0 @@
-import Slider from './modules/Slider.mjs';
-
-new Slider(8, 'slider__container').init();
---
+++ src/js/main.mjs
@@ -0,0 +1,4 @@
+import Slider from './modules/Slider.mjs';
+import '../css/style.css';
+
+new Slider(8, 'slider__container');
